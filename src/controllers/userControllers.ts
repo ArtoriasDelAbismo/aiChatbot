df5bb54327c232ba91bae3d1c1a7ecd36cad4734
Jeromino <jerojournade1@gmail.com>
2024-05-16 06:25:17 -0300

    add login/signup user validation

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import User from "../models/User.js";
-import { hash } from "bcrypt";
+import { hash, compare } from "bcrypt";
 
 
 async function getAllUsers(req: Request, res: Response, next: NextFunction) {
@@ -17,6 +17,8 @@ async function getAllUsers(req: Request, res: Response, next: NextFunction) {
 async function userSignup (req: Request, res: Response, next: NextFunction) {
   try {
     const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email })
+    if(existingUser) return res.status(401).send("User already registered");
     const hashedPassword = await hash(password, 10);
     const user = new User({ name, email, password: hashedPassword });
     await user.save();
@@ -28,4 +30,20 @@ async function userSignup (req: Request, res: Response, next: NextFunction) {
   }
 }
 
-export { getAllUsers, userSignup };
+async function userLogin (req: Request, res: Response, next: NextFunction) {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if(!user) return res.status(401).send("User not registered");
+    const isPasswordCorrect = await compare(password, user.password);
+    if(!isPasswordCorrect) return res.status(403).send("Incorrect Password")
+    
+    return res.status(200).json({ message: "OK", id: user._id.toString() });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(200).json({ message: "Not Found", cause: error.message });
+  }
+}
+
+export { getAllUsers, userSignup, userLogin };
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getAllUsers, userSignup } from "../controllers/userControllers.js";
-
+import { getAllUsers, userLogin, userSignup } from "../controllers/userControllers.js";
+import { validate, signupValidator, loginValidator } from "../utils/validator.js"
 const userRoutes = Router();
 
 userRoutes.get("/", getAllUsers);
-userRoutes.post("/signup", userSignup);
+userRoutes.post("/signup", validate(signupValidator), userSignup);
+userRoutes.post("/login", validate(loginValidator), userLogin);
+
 
 export default userRoutes;
